3c1f8b440c2a5cf5d1c00be91f066151df976edd
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-09-08 20:11:16 -0300

    Tercer test pasa

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -3,14 +3,11 @@ class Chopper
 	def chop (num , array)
 	
 		if array.include?(3)
-			
 			array.index{ |num| num == 3}
 
 		else
-
 			-1
 		
 		end
-
 	end
 end
--- chopper/spec/chopper_spec.rb
+++ chopper/spec/chopper_spec.rb
@@ -19,11 +19,11 @@ describe 'Chopper' do
 	it 'chop de 3 y [3] deberia ser 0' do
 		expect(chopper.chop(3,[3])).to eq 0 
 	end
-=begin
+
 	it 'chop de 3 y [0,7,3] deberia ser 0' do
 		expect(chopper.chop(3,[0,7,3])).to eq 2 
 	end
-
+=begin
 	it 'sum de [] deberia ser vacio' do
 		expect(chopper.sum([])).to eq 'vacio' 
 	end
