9622219993a16acafbdf3f01d01086b5924cb63e
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-09-17 21:05:14 -0300

    test: sum de [0,0] deberia ser cero (ademas cambios en implementacion de sum)

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -2,8 +2,8 @@ class Chopper
 	
 	def chop (num , array)
 	
-		if array.include?(3)
-			array.index{ |num| num == 3}
+		if array.include?(num)
+			array.index(num)
 		else
 			-1
 		end
@@ -13,23 +13,52 @@ class Chopper
 
 		if array.empty?
 			'vacio'
+		elsif sum_inside_array(array) > 99
+			'demasiado grande'
 		else
-			res = 0
-			array.each do |num| 
-				res += num
-			end
-			
-			if res == 1
-				'uno'			
-			elsif res == 4
-				'cuatro'
-			elsif res == 18
-				'uno,ocho'
-			elsif res == 100
-				'demasiado grande'
-			elsif res == 99
-				'nueve,nueve'													
-			end				
+			# Suma los numeros del array
+			num_result = sum_inside_array(array)
+			# Se descompone el numero en digitos dentro de un array
+			array_num_digit = descompose(num_result) 
+
+			numbers_string = []
+			# Se transforma el array de int en uno de string
+			array_num_digit.each do |digit|
+				numbers_string << hash_number_string(digit)
+			end 
+
+			return numbers_string.join(',')
 		end
+
+	end
+
+	def descompose(num)
+		ret = []
+		if num < 10
+			ret << num
+		else 
+			ret << num / 10
+			ret << num % 10
+		end
+		ret
+	end
+
+	def sum_inside_array(array)
+		res = 0
+		array.each do |num| 
+			res += num
+		end
+		res
+	end
+
+# Clave int (digito) y valor string (nombre del digito)
+	def hash_number_string(num)
+		num_string = {
+			0 => 'cero', 1 => 'uno', 2 => 'dos',
+			3 => 'tres', 4 => 'cuatro', 
+			5 => 'cinco', 6 => 'seis', 7 => 'siete',
+			8 => 'ocho', 9 => 'nueve'
+		}
+		num_string[num]
 	end
 end
--- chopper/spec/chopper_spec.rb
+++ chopper/spec/chopper_spec.rb
@@ -48,4 +48,18 @@ describe 'Chopper' do
 		expect(chopper.sum([50,49])).to eq 'nueve,nueve'
 	end
 
+# Parte agregada
+
+	it 'sum de [0,0] deberia ser cero' do
+		expect(chopper.sum([0,0])).to eq 'cero'
+	end
+=begin
+	it 'sum de [15] deberia ser uno,cinco' do
+		expect(chopper.sum([15])).to eq 'uno,cinco'
+	end
+
+	it 'sum de [20,1,1,1] deberia ser dos,tres' do
+		expect(chopper.sum([20,1,1,1])).to eq 'dos,tres'
+	end
+=end
 end
