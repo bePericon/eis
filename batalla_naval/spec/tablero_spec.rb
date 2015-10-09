e167f72b2fb64afcef1e471799beeeac4d3932b0
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-10-09 12:38:00 -0300

    Prueba de solucion de problema con cucumber

--- batalla_naval/spec/tablero_spec.rb
+++ batalla_naval/spec/tablero_spec.rb
@@ -4,7 +4,7 @@ require_relative '../model/tablero'
 describe 'Tablero' do
   let(:tablero) { Tablero.new(10,10)}
 
-  it 'Creacion del tablero' do
+  it 'Creacion del tablero de 10x10' do
     expect(tablero.medidas).to eq [10,10] 
   end
 
