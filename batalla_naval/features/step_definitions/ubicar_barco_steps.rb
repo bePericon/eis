42197dfaa3594c6ef228f912b1d1a17711f01357
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-10-12 17:24:35 -0300

    Agrego tercer Scenario: No puedo ubicar mi barco en una posición fuera del tablero

--- batalla_naval/features/step_definitions/ubicar_barco_steps.rb
+++ batalla_naval/features/step_definitions/ubicar_barco_steps.rb
@@ -10,29 +10,56 @@ When(/^elijo la posicion 'a3' vacia$/) do
 end
 
 Then(/^el barco se ubica en esa posicion correctamente$/) do
-  if @estado_posicion_elegida_crucero 
+  if @estado_posicion_elegida_crucero
     @juego.poner_barco(@barco_crucero, 'a3', 'horizontal')
+    print 'Se ubico el barco exitosamente!'
   end
 end
 
-Given(/^selecciono un barco 'submarino'$/) do
+###########################################################################################
+
+Given(/^selecciono un barco 'crucero y luego un 'destructor'$/) do
   @juego = BatallaNaval.new
-  @barco_submarino = @juego.elegir_barco('submarino')
+  @barco_crucero = @juego.elegir_barco('crucero')
+  @barco_destructor = @juego.elegir_barco('destructor')
 end
 
-When(/^elijo la posicion 'b3' que esta ocupada$/) do
-  @barco_crucero = @juego.elegir_barco('crucero')
+When(/^elijo la posicion 'a3' para 'crucero' y lo ubico,ademas elijo la posicion 'b3' que esta ocupada$/) do
   @estado_posicion_elegida_crucero = @juego.elegir_posicion(@barco_crucero, 'a3', 'horizontal')
   if @estado_posicion_elegida_crucero 
     @juego.poner_barco(@barco_crucero, 'a3', 'horizontal')
+    print 'Se ubico el barco exitosamente!'
   end
 
-  @estado_posicion_elegida_submarino = @juego.elegir_posicion(@barco_submarino, 'b3', 'vertical')
+  @estado_posicion_elegida_destructor = @juego.elegir_posicion(@barco_destructor, 'b3', 'vertical')
 end
 
-Then(/^el barco no se pudo ubicar en esa posicion$/) do
-  if @estado_posicion_elegida_submarino
-    @juego.poner_barco(@barco_submarino, 'b3', 'vertical')
+Then(/^el barco 'destructor' no se pudo ubicar en esa posicion$/) do
+  if @estado_posicion_elegida_destructor
+    @juego.poner_barco(@barco_destructor, 'b3', 'vertical')
+    print 'Se ubico el barco exitosamente!'
+  else
+    print 'No se puede ubicar, posicion ocupada!'
+  end
+    
+end
+
+###########################################################################################
+
+Given(/^selecciono un barco 'destructor' de nuevo$/) do
+  @juego = BatallaNaval.new
+  @barco_destructor = @juego.elegir_barco('destructor')
+end
+
+When(/^elijo la posicion 'k2' fuera del tablero$/) do
+  @estado_posicion_elegida_destructor = @juego.elegir_posicion(@barco_destructor, 'k2', 'horizontal')
+end
+
+Then(/^no puedo ubicar el barco en esa posicion que esta fuera del tablero$/) do
+  if @estado_posicion_elegida_destructor
+    @juego.poner_barco(@barco_destructor, 'k2', 'horizontal')
+    print 'Se ubico el barco exitosamente!'
+  else
+    print 'No se puede ubicar, posicion fuera de tablero'
   end
 end
-
--- batalla_naval/features/ubicar_barco.feature
+++ batalla_naval/features/ubicar_barco.feature
@@ -1,6 +1,7 @@
 Feature: Ubicar barco
   Como usuario quiero ubicar mis barcos
 
+
   Scenario: Ubico mi barco exitosamente
   Given selecciono un barco 'crucero'
   When elijo la posicion 'a3' vacia
@@ -8,12 +9,12 @@ Feature: Ubicar barco
 
   
   Scenario: No se puede ubicar un barco en una posición ya ocupada
-  Given selecciono un barco 'submarino'
-  When elijo la posicion 'b3' que esta ocupada
-  Then el barco no se pudo ubicar en esa posicion
+  Given selecciono un barco 'crucero y luego un 'destructor'
+  When elijo la posicion 'a3' para 'crucero' y lo ubico,ademas elijo la posicion 'b3' que esta ocupada
+  Then el barco 'destructor' no se pudo ubicar en esa posicion
 
-  @wip
+
   Scenario: No puedo ubicar mi barco en una posición fuera del tablero
-  Given selecciono un barco 'destructor'
+  Given selecciono un barco 'destructor' de nuevo
   When elijo la posicion 'k2' fuera del tablero
   Then no puedo ubicar el barco en esa posicion que esta fuera del tablero
