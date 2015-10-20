a798df55e329bbe3e7188b18708837cc903df6ab
Emmanuel Pericon <brianpericon.e92@gmail.com>
2015-10-19 22:48:02 -0300

    Refactor de repeticion de codigo

--- calculadora/app/app.rb
+++ calculadora/app/app.rb
@@ -17,93 +17,20 @@ module Ejemplo
       'hey! hola , esto va a ser una calculadora!'
     end
 
-    get 'suma' do
-      render 'calculadora_suma'
+    get ':operacion' do
+      render "#{params[:operacion]}"
     end
 
-    get 'resta' do
-      render 'calculadora_resta'
-    end
-
-    get 'promedio' do
-      render 'calculadora_promedio'
-    end
-
-    get 'multiplicacion' do
-      render 'calculadora_multiplicacion'
-    end
-
-    get 'division' do
-      render 'calculadora_division'
-    end
-
-
-    post 'suma' do
-
-      session[:primer_operando] = params[:primer_operando]
-      session[:segundo_operando] = params[:segundo_operando]
-      @primer_operando = session[:primer_operando]
-      @segundo_operando = session[:segundo_operando]
-
-      @resultado = CALCULADORA.sumar(@primer_operando.to_i, @segundo_operando.to_i)
+    post ':operacion' do
+      @resultado = CALCULADORA.send("#{params[:operacion]}", params[:primer_operando].to_i, params[:segundo_operando].to_i)
       @operaciones_realizadas = CALCULADORA.memoria
-      render 'calculadora_suma'
+      render "#{params[:operacion]}"
     end
 
-    post 'resta' do
-
-      session[:primer_operando] = params[:primer_operando]
-      session[:segundo_operando] = params[:segundo_operando]
-      @primer_operando = session[:primer_operando]
-      @segundo_operando = session[:segundo_operando]
-
-      @resultado = CALCULADORA.restar(@primer_operando.to_i, @segundo_operando.to_i)
-      @operaciones_realizadas = CALCULADORA.memoria
-      render 'calculadora_resta'
+    get 'resetear/:operacion' do
+      CALCULADORA.resetear_memoria
+      redirect_to "#{params[:operacion]}"
     end
-
-    post 'promedio' do
-
-      session[:primer_operando] = params[:primer_operando]
-      session[:segundo_operando] = params[:segundo_operando]
-      @primer_operando = session[:primer_operando]
-      @segundo_operando = session[:segundo_operando]
-
-      @resultado = CALCULADORA.promedio(@primer_operando.to_i, @segundo_operando.to_i)
-      @operaciones_realizadas = CALCULADORA.memoria
-      render 'calculadora_promedio'
-    end
-
-    post 'multiplicacion' do
-
-      session[:primer_operando] = params[:primer_operando]
-      session[:segundo_operando] = params[:segundo_operando]
-      @primer_operando = session[:primer_operando]
-      @segundo_operando = session[:segundo_operando]
-
-      @resultado = CALCULADORA.multiplicar(@primer_operando.to_i, @segundo_operando.to_i)
-      @operaciones_realizadas = CALCULADORA.memoria
-      render 'calculadora_multiplicacion'
-    end
-
-    post 'division' do
-
-      session[:primer_operando] = params[:primer_operando]
-      session[:segundo_operando] = params[:segundo_operando]
-      @primer_operando = session[:primer_operando]
-      @segundo_operando = session[:segundo_operando]
-
-      @resultado = CALCULADORA.dividir(@primer_operando.to_i, @segundo_operando.to_i)
-      @operaciones_realizadas = CALCULADORA.memoria
-      render 'calculadora_division'
-    end
-
-    #get 'resetear/:name' do
-    #  CALCULADORA.resetear_memoria
-    #  if ("#{params[:name]}" == "suma")
-    #    render 'calculadora_suma'
-    #  end
-    #end
 
   end
 end
